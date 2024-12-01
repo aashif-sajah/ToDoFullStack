1184e39be4cf6986d63b9b117e8fdedcebd91782
Aashif Sajah <masaashifsajah@gmail.com>
2024-12-01 21:07:19 +0530

    Tested Basic CRUD oparations

--- BackEnd/toDo/src/main/java/com/aashif/toDo/contoller/TaskController.java
+++ BackEnd/toDo/src/main/java/com/aashif/toDo/contoller/TaskController.java
@@ -1,7 +1,11 @@
 package com.aashif.toDo.contoller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 
@@ -12,5 +16,14 @@ public class TaskController
     public String helloWorld()
     {
         return "Hello World";
+    }
+
+    @PostMapping("/api/tasks")
+    public List<String> createTask()
+    {
+        List<String>users = new ArrayList<>();
+        users.add("John");
+        users.add("Jane");
+        return users;
     }
 }
